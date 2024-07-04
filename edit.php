9fbee934356358e3cb19c8e89853758094258b1c
new-flat <t.0826newflat@icloud.com>
2024-07-04 16:51:50 +0900

    フォームがPOSTメソッドで送信されたかバリデーション

--- edit.php
+++ edit.php
@@ -14,6 +14,11 @@ if (isset($_GET["id"])) {
         $edit_stmt->execute();
         $user = $edit_stmt->fetch(PDO::FETCH_OBJ); // 1件のデータを取得
 
+        if ($user->email === "0") {
+            $user->email = null;
+        }
+
+
         if (!$user) {
             $errors['id'] = $error_message5;
         }
@@ -98,6 +103,60 @@ if (isset($_GET['errors'])) {
                     </div>
                     <input type="date" name="editDate" value="<?php echo htmlspecialchars($errors['data']['editDate'] ?? $user->birth_date ?? '', ENT_QUOTES, 'UTF-8'); ?>">
                 </div>
+                <div>
+                    <div class="label">
+                        <label class="insertOption">メールアドレス</label>
+                        <p class="insertMust">必須</p>
+                    </div>
+                    <input type="text" name="editEmail" value="<?php echo htmlspecialchars($errors['data']['editEmail'] ?? $user->email ?? '', ENT_QUOTES, 'UTF-8'); ?>">
+                    <?php if (!empty($errors['messages']['editEmail'])) : ?>
+                        <p><?php echo $errors['messages']['editEmail']; ?></p>
+                    <?php endif; ?>
+                    <?php if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['editEmail'])) {
+                        $editEmail = $_POST['editEmail'];
+                            if (!filter_var($editEmail, FILTER_VALIDATE_EMAIL)) {
+                                echo "メールアドレスが正しくありません";
+                            }
+                        }         
+                    ?>
+                    
+                </div> 
+                <div>
+                   <div class="label">
+                        <label class="insertOption">通勤時間(分)</label>
+                   </div>  
+                   <input type="number" name="editCommute" value="<?php echo htmlspecialchars($errors['data']['editCommute'] ?? $user->commute_time ?? '', ENT_QUOTES, 'UTF-8'); ?>">
+                   <?php if (!empty($errors['messages']['editCommute'])) : ?>
+                        <p><?php echo $errors['messages']['editCommute']; ?></p>
+                    <?php endif; ?>  
+                </div>
+                <div>
+                    <div class="label">
+                        <label class="insertOption">血液型</label>
+                    </div>
+                    <div>
+                        <label><input type="radio" name="editBlood" value="A" <?php if (($errors['editBlood'] ?? $user->blood_type) == "A") {
+                                            echo "checked";}; ?>> A型</label>
+                        <label><input type="radio" name="editBlood"  value="B" <?php if (($errors['editBlood'] ?? $user->blood_type) == "B") {
+                                            echo "checked";}; ?>> B型</label>
+                        <label><input type="radio" name="editBlood"  value="O" <?php if (($errors['editBlood'] ?? $user->blood_type) == "O") {
+                                            echo "checked";}; ?>> O型</label>
+                        <label><input type="radio" name="editBlood"  value="AB" <?php if (($errors['editBlood'] ?? $user->blood_type) == "AB") {
+                                            echo "checked";}; ?>> AB型</label>
+                        <label><input type="radio" name="editBlood"  value="" <?php if (($errors['editBlood'] ?? $user->blood_type) == "") {
+                                            echo "checked";}; ?>> 不明</label>
+                    </div>
+                </div> 
+                <div>
+                    <div class="label">
+                        <label class="insertOption">既婚</label>
+                    </div>
+                    <div>
+                        <label><input type="radio" name="editMarried" value="1" <?php if (($errors['data']['editMarried'] ?? $user->married) == 1) {
+                                            echo "checked";}; ?>>既婚</label>
+                    </div>
+                </div>               
+                <!-- 保存ボタン -->
                 <input class="edit-submit" type="submit" value="保存" name="edit">
             </form>
         <?php endif; ?>
