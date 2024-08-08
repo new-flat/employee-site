e34c16c74904bcee4304236d43c45f85f7eb8e56
new-flat <t.0826newflat@icloud.com>
2024-08-08 11:04:44 +0900

    社員集計ページ追加

---
+++ controll/total_count.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once __DIR__ . '/../controll/branch_function.php';
+
+// DB接続
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=php-test', 'root', 'root', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+// 男女別、未登録の人数、その合計数を取得
+$gender_count_sql = "SELECT
+                        COUNT(CASE WHEN gender = 1 THEN 1 END) AS man,
+                        COUNT(CASE WHEN gender = 2 THEN 1 END) AS woman,
+                        COUNT(CASE WHEN gender IS NULL THEN 1 END) AS unknown,
+                        COUNT(*) AS total_gender
+                    FROM `php-test`";
+$countE_result = $pdo->query($gender_count_sql);
+$countE_row = $countE_result->fetch(PDO::FETCH_ASSOC);
+
+$man = $countE_row['man'];
+$woman = $countE_row['woman'];
+$unknown = $countE_row['unknown'];
+$total_gender = $countE_row['total_gender'];
+
+// 部門別の人数を取得
+$branch_count_sql = "SELECT branch, COUNT(*) AS employee_each_branch FROM `php-test` GROUP BY branch";
+$countB_result = $pdo->query($branch_count_sql);
+
+// 結果を格納
+$branch_counts = array_fill_keys(array_keys($branches), 0);
+
+// 結果表示
+while ($countB_row = $countB_result->fetch(PDO::FETCH_ASSOC)) {
+    $branchId = $countB_row['branch'];
+    $branch_counts[$branchId] = $countB_row['employee_each_branch'];
+}
+
+?>
---
+++ pages/total_view.php
@@ -0,0 +1,60 @@
+<?php
+require_once 'header.php';
+require_once __DIR__ . '/../controll/total_count.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>社員管理システム</title>
+    <link rel="stylesheet" href='/php_lesson/css/style.css'>
+</head>
+<body>
+    <div id="main" class="wrapper" style="padding-bottom: 50px;">
+        <div id="menu-title" class="wrapper">
+            <h1 class="title-name">社員集計</h1>
+        </div>
+        <h2 class="count_title">表1 :男女別社員数</h2>
+        <table class="count_table">
+            <thead>
+                <tr>
+                    <th class="table-title">性別</th>
+                    <th class="table-title">社員数</th>
+                </tr>
+                <tr>
+                    <td>男性</td>
+                    <td><?php echo $man; ?></td>
+                </tr>
+                <tr>
+                    <td>女性</td>
+                    <td><?php echo $woman; ?></td>
+                </tr>
+                <tr>
+                    <td>未登録</td>
+                    <td><?php echo $unknown; ?></td>
+                </tr>
+            </thead>
+        </table>
+
+        <h2 class="count_title">表2 :部門別社員数</h2>
+        <table class="count_table">
+            <thead>
+                <tr>
+                    <th class="table-title">部門</th>
+                    <th class="table-title">社員数</th>
+                </tr>
+            <?php foreach ($branches as $brancheId => $branch_name) : ?>
+                <?php $employee_each_branch = $branch_counts[$brancheId] ?>
+                <tr>
+                    <td><?php echo $branch_name; ?></td>
+                    <td><?php echo "$employee_each_branch\n"; ?></td>
+                </tr>
+            <?php endforeach; ?>    
+            </thead>
+        </table>
+    </div>
+</body>
+</html>
