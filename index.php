12b6241254a7ffb619d6bd2292d41d69eafa6d9b
new-flat <t.0826newflat@icloud.com>
2024-10-29 14:58:35 +0900

    テスト

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (isset($_POST['logout'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>社員管理システム</title>
+    <title>社員用管理システム</title>
     <link rel="stylesheet" href='/employee_site/css/style.css'>
 </head>
 <body>
